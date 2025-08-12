074dbb0639fe08260e5c52e651ac57c6d70fff93
Pedro Salemi <pedrosalemi@gmail.com>
2025-08-12 20:11:18 -0300

    Esqueci de salvar

--- js/script.js
+++ js/script.js
@@ -17,7 +17,7 @@ function toggleMenu() {
 
 /* ----- TYPING EFFECT ----- */
 var typingEffect = new Typed(".typedText", {
-    strings: ["Estagiário","Desenvolvedor","Estudante"],
+    strings: ["Desenvolvedor","Programador","de TI"],
     typeSpeed: 100,
     backSpeed: 80,
     backDelay: 2000,
