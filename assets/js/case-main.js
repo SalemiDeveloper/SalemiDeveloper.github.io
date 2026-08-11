abbd67debd129399e9c36e134fded4339e4aa929
Pedro Salemi <pedrosalemi@gmail.com>
2026-08-11 00:39:49 -0300

    fix: Correção no padding do topo da página.

--- assets/js/case-main.js
+++ assets/js/case-main.js
@@ -2,14 +2,19 @@ import { getCase } from "./services/cases.js";
 import { CasePage } from "./components/case-page.js";
 import { Layout } from "./components/layout.js";
 import { Navbar } from "./components/navbar.js";
-import { initializeScrollRevel } from "./utils/scroll-reveal.js";
-import { initializeTheme, initializeThemeToggle } from "./services/theme.js";
+import {
+    initializeTheme,
+    initializeThemeToggle
+} from "./services/theme.js";
+
+initializeTheme();
 
 async function render() {
     const params = new URLSearchParams(window.location.search);
     const slug = params.get("project");
     const project = await getCase(slug);
     app.innerHTML = CasePage(project);
+    initializeThemeToggle();
 }
 
 render();
--- assets/js/components/case-page.js
+++ assets/js/components/case-page.js
@@ -7,16 +7,23 @@ import { ArchitectureSection } from "./architecture-section.js";
 import { ChallengesSection } from "./challenge-section.js";
 import { GallerySection } from "./gallery-section.js";
 import { Button } from "./button.js";
+import { ThemeToggle } from "./theme-toggle.js";
 
 export function CasePage(project) {
     return `
         <section class="case">
             <div class="container">
-           ${Button({
-                text: "← Voltar para os projetos",
-                href: "index.html#projects",
-                variant: "ghost"
-            })}
+
+                <div class="case__topbar">
+                    ${Button({
+                        text: "← Voltar para os projetos",
+                        href: "index.html#projects",
+                        variant: "secondary"
+                    })}
+
+                    ${ThemeToggle()}
+                </div>
+
                 ${CaseHeader(project)}
                 ${OverviewSection(project)}
                 ${RoleSection(project)}
@@ -25,8 +32,8 @@ export function CasePage(project) {
                 ${ArchitectureSection(project)}
                 ${ChallengesSection(project)}
                 ${GallerySection(project)}
+
             </div>
         </section>
     `;
-
 }
