4997de87227dc69eede8eaabd4fad7a1b83ca7ce
Pedro Salemi <pedrosalemi@gmail.com>
2026-08-19 22:03:15 -0300

    feat: Adição do vídeo demonstrando a aplicação.

--- assets/js/components/gallery-section.js
+++ assets/js/components/gallery-section.js
@@ -3,17 +3,25 @@ import { GalleryCard } from "./gallery-card.js";
 export function GallerySection(project) {
     return `
         <section class="gallery">
+
             <h2 class="gallery__title">
-                Demosntração da Aplicação
+                Demonstração da Aplicação
             </h2>
 
-            <p class="gallery__notice">
-                Infelizmente a RAWG API aparentemente está fora do ar. Enquanto desenvolvia o projeto estava ok, porém no final de semana que eu ia gravar a aplicação funcionando já era tarde demais.
-            </p>
+            <div class="gallery__video">
+                <iframe
+                    src="https://www.youtube.com/embed/hL-82-Rqu-4?si=lm9FgO50dW68kAej"
+                    title="Demonstração do Game Tracker"
+                    frameborder="0"
+                    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                    allowfullscreen
+                ></iframe>
+            </div>
 
             <div class="gallery__grid">
                 ${project.gallery.map(GalleryCard).join("")}
             </div>
+
         </section>
     `;
 }
